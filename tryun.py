9f0b1f41392465bac5570813db82c4a9a909be6b
dziem <dziemboh@gmail.com>
2019-04-21 16:37:49 +0700

    Bit improvement
    Better params and result
    Bit better code
    Added comments

--- tryun.py
+++ tryun.py
@@ -12,9 +12,9 @@ for i in x:
 #const
 learning_rate = 1
 discount_rate = 0.9
-min_step = 28
+min_step = 28 #minimum possible step to reach 15,15 (500)
 max_step = min_step * 20
-learn = 100 #times
+learn = 200 #times
 #function
 def movemate(x_pos, y_pos):
 	while True: #generate move and check if hit a wall, if hit a wall, generate new move
@@ -31,46 +31,41 @@ def movemate(x_pos, y_pos):
 	return move
 def learnmate(t): #learning rate decreases over time
 	return learning_rate * math.exp( -1 * (t / learn) )
-#the learn table thing 
-map = [] 
+#the learn table thing or q-table to be exact
+map = [] #map = q-table
 for i in range (0, 15):
 	new = []
 	for j in range (0, 15):
 		new2 = []
 		for k in range (0, 4):
-			foo = 0
+			if(i == 0 and k == 0): #if top and move north
+				foo = -99999 #make the map out of map move disabled and prevent error
+			elif(i == 14 and k == 2): #if bottom and move south
+				foo = -99999 #make the map out of map move disabled and prevent error
+			elif(j == 0 and k == 3): #if left and move west
+				foo = -99999 #make the map out of map move disabled and prevent error
+			elif(j == 14 and k == 1): #if right and move east
+				foo = -99999 #make the map out of map move disabled and prevent error
+			else:
+				foo = 0 #init zero
 			new2.append(foo)
 		new.append(new2)
 	map.append(new)
-#make the map out of map move disabled
-for i in range (0, 15):
-	new = []
-	for j in range (0, 15):
-		new2 = []
-		for k in range (0, 4):
-			if(i == 0 and k == 0):
-				map[i][j][k] = -99999
-			elif(i == 14 and k == 2):
-				map[i][j][k] = -99999 
-			elif(j == 0 and k == 3):
-				map[i][j][k] = -99999 
-			elif(j == 14 and k == 1):
-				map[i][j][k] = -99999 
 #algorithm
 i = 0
-target = 0
+#target = 0 #try bits ; how many times the episode reach 15,15
 while i < learn:
-	step = random.randint(min_step,max_step)
-	x_pos = 14
-	y_pos = 0
+	step = random.randint(min_step,max_step) #how long an episode is
+	x_pos = 14 #start x
+	y_pos = 0 #start y
 	j = 0
 	while j < step:
 		randomate = random.random()
 		if(randomate < learnmate(i)):
-			move = movemate(x_pos, y_pos)
+			move = movemate(x_pos, y_pos) #explore
 			#print('move/random')
 		else:
-			move = np.argmax(map[x_pos][y_pos])
+			move = np.argmax(map[x_pos][y_pos]) #exploit
 			#print('test')
 		if(move == 0):
 			next_x = x_pos - 1
@@ -87,23 +82,23 @@ while i < learn:
 		map[x_pos][y_pos][move] = map[x_pos][y_pos][move] + learnmate(i) * (int(z[next_x][next_y]) + (discount_rate * np.max(map[next_x][next_y])) - map[x_pos][y_pos][move])
 		x_pos = next_x
 		y_pos = next_y
-		if(x_pos == 0 and y_pos == 14): #target reached
-			target += 1
+		if(x_pos == 0 and y_pos == 14): #target reached ; finish episode
+			#target += 1
 			break
 		j += 1
 	i += 1
 #testy bits
 #print(target)
-x_pos = 14
-y_pos = 0
+x_pos = 14 #start x
+y_pos = 0 #start y
 score = 0
 j = 0
 #print(map)
-
+#the actual try to get the 15,15
 while True:
 	score += int(z[x_pos][y_pos])
 	move = np.argmax(map[x_pos][y_pos])
-	#print(x_pos, y_pos)
+	#print(x_pos, y_pos) #trace bits
 	if(move == 0):
 		x_pos  -= 1
 	elif(move == 1):
@@ -112,14 +107,14 @@ while True:
 		x_pos += 1
 	elif(move == 3):
 		y_pos -= 1
-	if(x_pos == 0 and y_pos == 14):
+	if(x_pos == 0 and y_pos == 14): #target reached
 		score += int(z[x_pos][y_pos])
 		print('target reached')
 		break
 	j += 1
-	#print(move)
-	#print(score)
-	if(j > 1000):
+	#print(move) #trace bits
+	#print(score) #trace bits
+	if(j > 1000): #to stop the infinite loop thing
 		print('timeout')
 		break
 print('final score : ',score)
